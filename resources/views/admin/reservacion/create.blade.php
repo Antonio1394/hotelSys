ebb203ca541c13740eea6fea7f04f0eaebd66a3a
antonio1394 <antoniorivm1394@gmail.com>
2017-11-03 23:20:39 -0600

    [update]

--- resources/views/admin/reservacion/create.blade.php
+++ resources/views/admin/reservacion/create.blade.php
@@ -1,7 +1,5 @@
 {!! Form::open(['route' => 'admin.reservacion.store', 'method' => 'POST', 'class' => 'form-horizontal', 'id' => 'createForm', 'data-parsley-validate' => '',]) !!}
     @include('admin.reservacion.inputs')
-
-
     <div class="modal-footer">
         {!! Form::submit('Crear', ['class' => 'btn btn-primary waves-effect waves-light']) !!}
         <button type="button" class="btn btn-info waves-effect" id="btnBuscar">Buscar</button>
