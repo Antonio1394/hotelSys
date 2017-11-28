8b63d6f5c8854f181d9e5f266446431033cbab06
antonio1394 <antoniorivm1394@gmail.com>
2017-11-27 23:08:41 -0600

    [update]

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,10 +19,6 @@ Route::post('auth/verify', 'Auth\AuthController@verifyLogin');
 
 
 
-
-
-
-
 Route::group(['prefix' => 'admin', 'namespace' => '\Admin', 'middleware' => ['auth']], function() {
 
     Route::get('/', 'PrincipalController@index');
