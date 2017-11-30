4d1bcfa11dd868810231f039f46101084a888428
antonio1394 <antoniorivm1394@gmail.com>
2017-11-30 12:51:35 -0600

    [add] table inventario :cat:

---
+++ app/Models/Inventario.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Inventario extends Model
+{
+    protected $table='inventario';
+    protected $fillable=['producto','marca','cantidad'];
+}
