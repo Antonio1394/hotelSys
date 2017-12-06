b1773f58335bbd420d6049393bc1fbd3220afb2c
antonio1394 <antoniorivm1394@gmail.com>
2017-12-05 23:16:53 -0600

    [add] formulario crear

--- resources/views/admin/item/create.blade.php
+++ resources/views/admin/item/create.blade.php
@@ -4,5 +4,8 @@
         {!! Form::submit('Crear', ['class' => 'btn btn-primary waves-effect waves-light']) !!}
         <button type="button" class="btn btn-default waves-effect" data-dismiss="modal">Cerrar</button>
     </div>
+
+
+    
 {!! Form::close() !!}
 {!! Html::script('assets/own/js/item.js') !!}
