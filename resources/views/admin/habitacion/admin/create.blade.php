451135d6d923e3f11073959889b94c9f08925dff
antonio1394 <antoniorivm1394@gmail.com>
2018-02-14 23:05:35 -0600

    [add]

--- resources/views/admin/habitacion/admin/create.blade.php
+++ resources/views/admin/habitacion/admin/create.blade.php
@@ -3,6 +3,10 @@
     <div class="modal-footer">
         {!! Form::submit('Crear', ['class' => 'btn btn-primary waves-effect waves-light']) !!}
         <button type="button" class="btn btn-default waves-effect" data-dismiss="modal">Cerrar</button>
+
+
+
+
     </div>
 {!! Form::close() !!}
 {!! Html::script('assets/own/js/adminH.js') !!}
